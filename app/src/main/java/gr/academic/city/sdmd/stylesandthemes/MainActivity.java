e01eae57f1606a94924cc2491d626359caf3eb2e
Ivo Neskovic <ivo.neskovic@gmail.com>
2020-03-01 21:04:50 +0100

    upgraded gradle and migrated to androidx.

--- app/src/main/java/gr/academic/city/sdmd/stylesandthemes/MainActivity.java
+++ app/src/main/java/gr/academic/city/sdmd/stylesandthemes/MainActivity.java
@@ -1,6 +1,6 @@
 package gr.academic.city.sdmd.stylesandthemes;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
